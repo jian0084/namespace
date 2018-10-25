75b8b9f8141fd7a07b956203ce4aad2a093c77e6
Joyce84 <jian0084@algonquinlive.com>
2018-10-25 10:46:35 -0400

    Update namespace.js
    change date

---
+++ namespace.js
@@ -0,0 +1,27 @@
+/********************************
+Filename: namespace.js
+Author: @Joyce
+Description: 
+Date: Oct 25, 2018
+*********************************/
+
+var JIAN0084 = {
+    init: function () {
+
+        let div = document.createElement("div");
+        div.textContent = "jian0084";
+        div.className = "box";
+        document.querySelector("#boxes").appendChild(div);
+        
+        div.addEventListener("mouseover", function () {
+            div.classList.toggle("highlight");
+        })
+        div.addEventListener("mouseout", function () {
+            div.classList.toggle("highlight");
+        })
+        div.addEventListener("click", function () {
+            div.style.borderColor = "blue";
+            div.style.backgroundColor = "black";
+        })
+    }
+};
