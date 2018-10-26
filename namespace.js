36c2dddec55b200e38ea762abf59f0c58afd7b18
jian0084 <jian0084@algonquinlive.com>
2018-10-26 10:38:22 -0400

    Update namespace.js
    add setTimeout

--- namespace.js
+++ namespace.js
@@ -12,18 +12,24 @@ var JIAN0084 = {
         div.textContent = "jian0084";
         div.className = "box";
         document.querySelector("#boxes").appendChild(div);
-        
-        
-        div.addEventListener("click", function() {
+
+
+//        div.addEventListener("click", function () {
+//            div.style.borderColor = "yellow";
+//            div.style.backgroundColor = "darkgray";
+//        }, 1000);
+
+        div.addEventListener("click", setTimeout(function () {
             div.style.borderColor = "yellow";
             div.style.backgroundColor = "darkgray";
-        }, 1000);
+        }, 1000));
+
         div.addEventListener("mouseover", function (e) {
             e.target.classList.toggle("highlight");
         })
         div.addEventListener("mouseout", function (e) {
             e.target.classList.toggle("highlight");
         })
-        
+
     }
 };
